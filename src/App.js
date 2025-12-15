97a457c89c9b5482eea16acb8ff498d0442aaeaa
Nishar Ahmad <ahmadnishar1132@gmail.com>
2025-12-15 12:40:19 +0530

    modification in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
+
 import About from "./components/About";
 import Blog from "./components/Blog";
 import Contact from "./components/Contact";
@@ -19,20 +19,26 @@ function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <div className="App">
+
+      {/* ⬇️ FULL HEIGHT FLEX LAYOUT */}
+      <div className="min-h-screen flex flex-col bg-[rgb(33,32,32)] text-white">
         <Navbar />
-        <div className="main-content"></div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/testimonials" element={<Testimonials />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+
+        {/* ⬇️ EXPANDING CONTENT */}
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/testimonials" element={<Testimonials />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+
         <Footer />
       </div>
     </BrowserRouter>
